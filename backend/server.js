0d53997fe65b48227733cf7e7615a52b733a0d5c
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-01-30 22:30:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,2 @@
-const express = require("express");
-const cors = require("cors");
-
-const travelRoutes = require("./routes/travelRoutes");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// Route connection
-app.use("/api/travel", travelRoutes);
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("UBoundFree Backend Running 🚀");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+const wishlistRoutes = require("./routes/wishlistRoutes");
+app.use("/api/wishlist", wishlistRoutes);
