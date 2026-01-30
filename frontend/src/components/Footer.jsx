243dc78e50b4616df753a122458c002e3463276f
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-01-30 22:23:24 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,1 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "15px", background: "#1e3d59", color: "white" }}>
+      © 2026 UBoundFree
+    </footer>
+  );
+}
 
+export default Footer;
