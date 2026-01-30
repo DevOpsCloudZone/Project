9a62534c9a7a39d634390bc999003dc8259ac862
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-01-30 22:24:42 +0530

    Update Food.jsx

--- frontend/src/pages/Food.jsx
+++ frontend/src/pages/Food.jsx
@@ -1,1 +1,10 @@
+function Food() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Food Spots 🍔</h1>
+      <p>Discover best food experiences.</p>
+    </div>
+  );
+}
 
+export default Food;
