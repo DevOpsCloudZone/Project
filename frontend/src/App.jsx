5eafcef9dc288598635019ee92652e4feb99b6b1
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-01-30 22:22:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,26 @@
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Travel from "./pages/Travel";
+import Food from "./pages/Food";
+import Nature from "./pages/Nature";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/travel" element={<Travel />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/nature" element={<Nature />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
