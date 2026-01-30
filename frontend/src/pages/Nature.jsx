a9925c07fa6d217c180d67c8f827ecaee7d3fda3
A.Umesh <108319125+Aveti-Umesh@users.noreply.github.com>
2026-01-30 22:24:59 +0530

    Update Nature.jsx

--- frontend/src/pages/Nature.jsx
+++ frontend/src/pages/Nature.jsx
@@ -1,1 +1,11 @@
 
+function Nature() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Nature Places 🌿</h1>
+      <p>Explore natural wonders.</p>
+    </div>
+  );
+}
+
+export default Nature;
